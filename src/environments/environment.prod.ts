8461a373c8b0420e18b655e9b51d09df12af48c6
Suruchi Verma <Suruchi.Verma@synechron.com>
2021-05-10 12:01:59 +0530

    ui code changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
  export const environment = {
   production: true,
-  baseUrl: 'http://3.88.149.148:8080/eCare/v4/' 
+  baseUrl: 'http://54.164.243.1:8080/eCare/v4/' 
 }; 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://3.88.149.148:8080/eCare/v4/'  
+  baseUrl: 'http://54.164.243.1:8080/eCare/v4/'  
 };
 /*
  * For easier debugging in development mode, you can import the following file
